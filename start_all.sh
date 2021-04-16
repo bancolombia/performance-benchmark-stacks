1ab8bf3139576d52c50be3169e6d38aeaea49d77
Juan Carlos Galvis <juanc.galvis@outlook.com>
2021-04-16 18:48:48 -0500

    fix nodejs dockerfiles

--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 stacks=("nodejs/express" "nodejs/nestjs" "java/spring-boot-tomcat" "java/spring-boot-webflux" "elixir/plug-cowboy")
+#stacks=("java/spring-boot-tomcat")
 scenarios=("health-check" "cipher-bcrypt" "hash-sha256")
 
 for stack in "${stacks[@]}"; do
