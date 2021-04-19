bd94bc8ceb815cab5e0efc4311d3eca52a0121f8
Santiago Garcia Gil <sagarci@bancolombia.com.co>
2021-04-19 17:11:30 -0500

    Go/gin change path to query (#7)
    * init project with gin and health check
    
    * generate hash
    
    * temp
    
    * fix hash
    
    * all scenarios go with gin
    
    * execute 1 time the function if delay is equals to 0
    
    * format
    
    * alter path to query

--- go/gin/httpd/main.go
+++ go/gin/httpd/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/status", handler.Healthy)
-	r.GET("/hash/:percentage/:delay", handler.Hash)
-	r.GET("/crypt/:percentage/:delay", handler.Crypt)
+	r.GET("/hash", handler.Hash)
+	r.GET("/crypt", handler.Crypt)
 	r.Run()
 }
