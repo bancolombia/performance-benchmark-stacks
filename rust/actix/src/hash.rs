fc4f6c0d0dbe22860ca5871b047e00cbc996bec7
Juan Carlos Galvis <juanc.galvis@outlook.com>
2021-04-20 08:49:44 -0500

    complete hex format

--- rust/actix/src/hash.rs
+++ rust/actix/src/hash.rs
@@ -1,12 +1,12 @@
 use std::str;
 
+use hex::encode;
 use log::{info, trace, warn};
 use sha2::{Digest, Sha256};
 
-pub fn sha_256(plain: String) -> &'static str {
+pub fn sha_256(plain: String) -> String {
     let mut hasher = Sha256::new();
     hasher.update(plain);
-    let res = hasher.finalize();
-    // TODO: parse res to hex
-    return "hex here";
+    let res = &hasher.finalize()[..];
+    return encode(res);
 }
