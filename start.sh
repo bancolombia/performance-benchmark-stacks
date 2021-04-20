460e8baf1c9116fe0969e0afac08de5f3772f7dc
Santiago Garcia <sagarci@bancolombia.com.co>
2021-04-20 08:48:55 -0500

    scripts to deploy

--- start.sh
+++ start.sh
@@ -108,7 +108,7 @@ waitForTcp() {
 
 waitForUrl() {
   echo "Testing $1"
-  timeout -s TERM 45 bash -c \
+  bash -c \
     'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
     do echo "Waiting for ${0}" && sleep 2;\
     done' ${1}
