2f600f7addf7f716c36a009b7a2c361755283196
Santiago Garcia <sagarci@bancolombia.com.co>
2021-04-20 12:12:18 -0500

    enable bds

--- reports/front/src/app/components/app/app.component.ts
+++ reports/front/src/app/components/app/app.component.ts
@@ -1,22 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {ResultsService} from "../../services/results.service";
+import { Component, OnInit } from '@angular/core';
+import { ResultsService } from '../../services/results.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'front';
   scenarios = [];
   charts: any;
+  modeText = 'Enable Light Mode!';
 
-  constructor(private dataService: ResultsService) {
-  }
+  constructor(private dataService: ResultsService) {}
 
   ngOnInit(): void {
-    this.dataService.loadResults()
-      .subscribe(data => this.buildCharts(data));
+    this.dataService.loadResults().subscribe((data) => this.buildCharts(data));
   }
 
   public scenarioData(scenario): any {
@@ -28,5 +27,24 @@ export class AppComponent implements OnInit {
     this.scenarios = Object.keys(data);
   }
 
-}
+  switchTheme(checked) {
+    var link = document.createElement('link');
+    if (checked) {
+      this.modeText = 'Disabled Dark Mode!';
+      document.documentElement.setAttribute('data-theme', 'dark');
+      link.href =
+        'https://galatea-dev.apps.ambientesbc.com/2.0.0/bds-black.min.css';
+    } else {
+      this.modeText = 'Enable Dark Mode!';
 
+      document.documentElement.setAttribute('data-theme', 'light');
+
+      link.href = 'https://galatea-dev.apps.ambientesbc.com/2.0.0/bds.min.css';
+    }
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.media = 'screen,print';
+    document.getElementsByTagName('head')[0].lastChild.remove();
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+}
