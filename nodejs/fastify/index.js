038de9f02dfecf3e1a8e2346d53158c20149569a
Ivan Jaimes <ivanjj22@hotmail.com>
2021-09-01 17:34:44 -0500

    Add .dockerignore

--- nodejs/fastify/index.js
+++ nodejs/fastify/index.js
@@ -7,7 +7,8 @@ init(fastify);
 
 const start = async () => {
   try {
-    await fastify.listen(port)
+    await fastify.listen(port, '0.0.0.0');
+    console.log(`Server started in port ${port}`);
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
